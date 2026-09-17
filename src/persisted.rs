a48ec069873dc89a5376179c82ea9da0ab505c2b
FlorianRoudaut <florian.roudaut@ubisoft.com>
2026-09-17 17:31:21 +0200

    Reviewed the file structure

--- src/persisted.rs
+++ src/persisted.rs
@@ -2,7 +2,7 @@ use crate::{Key, PersistenceError};
 
 pub trait Persisted: Sized {
     fn key(&self) -> &Key;
-    fn persisted_type(&self) -> &str;
+    fn persisted_type() -> &'static str;
     fn to_proto_bytes(items: &[Self]) -> Vec<u8>;
     fn from_proto_bytes(bytes: &[u8]) -> Result<Vec<Self>, PersistenceError>;
 }
