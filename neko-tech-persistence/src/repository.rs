769941a484f2f7e36490ef4d4b477f04eb870366
FlorianRoudaut <florian.roudaut@ubisoft.com>
2026-09-17 16:19:49 +0200

    updated repository with different read operations

--- neko-tech-persistence/src/repository.rs
+++ neko-tech-persistence/src/repository.rs
@@ -9,7 +9,9 @@ pub enum PersistenceError {
 
 pub trait Repository<T: Persisted> {
     fn create(&mut self, item: T) -> Result<(), PersistenceError>;
-    fn read(&self, key: &Key) -> Result<&T, PersistenceError>;
+    fn read_one_by_id(&self, key: &Key) -> Result<&T, PersistenceError>;
+    fn read_one_by_name(&self, unique_name: &str) -> Result<&T, PersistenceError>;
+    fn read_all(&self) -> Vec<&T>;
     fn update(&mut self, item: T) -> Result<(), PersistenceError>;
     fn delete(&mut self, key: &Key) -> Result<(), PersistenceError>;
 }
